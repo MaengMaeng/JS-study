8521e59175bf57d1d2e56d47a035385a2a516450
MaengMaeng <maybe8189@naver.com>
2020-07-28 23:55:21 +0900

    레벨 체크 2 / solution01 추가

---
+++ algorithm/programmers/level-check-2/1/solution01.js
@@ -0,0 +1,29 @@
+function solution(land){
+    let r = land.length;
+
+    let dp = new Array(r+1);
+
+    for(let i = 0; i < r+1; i++){
+        dp[i] = Array.from({length: 4}, () => 0);
+    }
+
+    for(let i = 0; i < 4; i++){
+        dp[0][i] = land[0][i];
+    }
+
+    for(let i = 1; i < r; i++){
+        for(let j = 0; j < 4; j++){
+            for(let k = 0; k < 4; k++){
+                if(j !== k){
+                    dp[i][j] = Math.max(dp[i][j], land[i][j] + dp[i-1][k]);
+                }
+            }
+        }
+    }
+
+    return Math.max.apply(null, dp[r-1]);
+}
+
+let land = [[1,2,3,5],[5,6,7,8],[4,3,2,1]];
+
+console.log(solution(land));
---
+++ algorithm/programmers/level-check-2/1/test.js
@@ -0,0 +1,3 @@
+let land = [[1,2,3,5],[5,6,7,8],[4,3,2,1]];
+
+console.log(solution(land));
