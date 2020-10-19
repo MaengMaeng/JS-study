b8a542c015e3ba36b4eec4ddf7de71fd6d5ba83e
MaengMaeng <maybe8189@naver.com>
2020-10-20 08:31:09 +0900

    programmers 49189, readme.md 추가

---
+++ algorithm/study/49189/solution.js
@@ -0,0 +1,41 @@
+function solution(n, edge) {
+    let answer = 0, dist;
+
+    const graph = [];
+    for(let i = 0; i < n; i++){
+        graph.push([]);
+    }
+    for(let i = 0; i < edge.length; i++){
+        graph[edge[i][0] - 1].push(edge[i][1] - 1);
+        graph[edge[i][1] - 1].push(edge[i][0] - 1);
+    }
+
+    const dists = Array.from({length : n}, () => Infinity);
+    dists[0] = 1;
+
+    const queue = [0];
+
+    while(queue.length > 0){
+        let node = queue.shift();
+
+        for(let i = 0; i < graph[node].length; i++){
+            if(dists[graph[node][i]] > dists[node] + 1){
+                dists[graph[node][i]] = dists[node] + 1;
+                queue.push(graph[node][i]);
+                dist = dists[node] + 1;
+            }
+        }
+    }
+
+    for(let i = 0; i < dists.length; i++){
+        if(dists[i] === dist) answer++;
+    }
+
+    return answer;
+}
+
+const input = [6, [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]	 ];
+// const input = [7, [[3, 6], [4, 3], [3, 2], [1, 2], [2, 4], [5, 2], [6,7], [5,7]]	 ];
+// const input = [3, [[1,2], [1,3]]];
+
+console.log(solution(...input));
