6c93aa1591dd2de5003e08f6b10aa577e45ea24f
MaengMaeng <maybe8189@naver.com>
2020-10-22 20:56:11 +0900

    programmers 42897, 42898 java로 해결

---
+++ algorithm/study/42897/solution.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] money) {
+    	int length = money.length;
+    
+        int[][] newMoneys = {Arrays.copyOfRange(money, 1, length), Arrays.copyOfRange(money, 0, length - 1), Arrays.copyOfRange(money, 1, length - 1)};
+        int[] results = new int[3];
+        
+        for(int i = 0; i < 3; i++) {
+        	results[i] = dp(newMoneys[i]);
+        }
+        
+        Arrays.sort(results);
+        
+        return results[2];
+    }
+    
+    public int dp(int[] money) {
+        int length = money.length;
+
+        int[] dp = new int[length];
+        boolean[] check = new boolean[length];
+        
+        dp[0] = money[0];
+        check[0] = true;
+        dp[1] = money[0] > money[1] ? money[0] : money[1];
+        check[1] = money[1] > money[0];
+        		
+        for(int i = 2; i < length; i++) {
+        	int a = dp[i - 1];
+        	int b = dp[i - 2] +  money[i];
+        	
+        	if(a > b) {
+        		dp[i] = a;
+        	}
+        	else {
+        		dp[i] = b;
+        		check[i] = true;
+        	}
+        }
+                
+        return dp[length - 1];
+    }
+}
