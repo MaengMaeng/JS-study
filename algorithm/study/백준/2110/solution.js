a8783b91bb5b31f1b4d419c89a183e2e4392e9fb
MaengMaeng <maybe8189@naver.com>
2020-11-15 23:07:32 +0900

    코드 리팩토링

--- algorithm/study/백준/2110/solution.js
+++ algorithm/study/백준/2110/solution.js
@@ -3,40 +3,40 @@ let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 // let input = fs.readFileSync('./dev/stdin.txt').toString().split('\n');
 
 let [N, C] = input[0].split(' ').map(v => v * 1);
-let numbers = [];
+let house = [];
 
 for (let i = 1; i < input.length; i++) {
   if (input[i] !== '') {
-    numbers.push(input[i] * 1);
+    house.push(input[i] * 1);
   }
 }
 
-numbers.sort((a,b) => a-b);
+house.sort((a,b) => a-b);
 
 // console.log(N, C);
 // console.log(numbers);
 
-const binarySearch = (distance, numbers, n) => {
+const binarySearch = (distance, house, n) => {
     let answer = 0;
 
     let left = 1, right = distance, mid = 0;
 
     while(left <= right){
-        let cnt = 0;
-        let prev = numbers[0];
+        let count = 0;
+        let prev = house[0];
 
         mid = Math.floor((left + right) / 2);
 
         for(let i = 1; i < N; i++){
-            if(numbers[i] - prev < mid){
-                cnt++;
+            if(house[i] - prev < mid){
+                count++;
             }
             else{
-                prev = numbers[i];
+                prev = house[i];
             }
         }
 
-        if(cnt <= n){
+        if(count <= n){
             answer = mid > answer ? mid : answer;
             left = mid + 1;
         }
@@ -48,7 +48,7 @@ const binarySearch = (distance, numbers, n) => {
     return answer;
 }
 
-let distance = numbers[N - 1];
+let distance = house[N - 1];
 let n = N - C;
 
-console.log(binarySearch(distance, numbers, n));
+console.log(binarySearch(distance, house, n));
--- algorithm/study/백준/2512/solution.js
+++ algorithm/study/백준/2512/solution.js
@@ -3,30 +3,29 @@ let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 // let input = fs.readFileSync('./dev/stdin.txt').toString().split('\n');
 
 let N = input[0] * 1;
-let numbers = input[1].split(' ').map((v) => v*1);
+let budgets = input[1].split(' ').map((v) => v*1);
 let Limit = input[2] * 1;
 
-numbers.sort((a,b) => a-b);
+budgets.sort((a,b) => a-b);
 
 const binarySearch = () => {
     let left = 0, right = Limit;
     let answer = 0;
 
-    let h = 0, sum = 0;
+    let sum = 0;
     for(let i = 0; i < N; i++){
-        h = h > numbers[i] ? h : numbers[i];
-        sum += numbers[i];
+        sum += budgets[i];
     }
 
-    if(sum <= Limit) return h;
+    if(sum <= Limit) return budgets[budgets.length - 1];
 
     while(left <= right){
         let mid = Math.floor((left + right) / 2);
         let sum = 0;
                 
         for(let i = 0; i < N; i++){
-            if(mid > numbers[i]){
-                sum += numbers[i];
+            if(mid > budgets[i]){
+                sum += budgets[i];
             }
             else{
                 sum += mid;
